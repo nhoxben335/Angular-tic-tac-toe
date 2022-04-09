e6ebb01e7382fd1f50da7696f4fbd4bad652d0d6
Brooke Grover <brooketg@hotmail.com>
2022-04-09 15:54:19 -0300

    tictactoe app

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: `
+      <button nbButton *ngIf="!value">{{ value }}</button>
+      <button nbButton hero status="warning" *ngIf="value == 'X'">{{ value }}</button>
+      <button nbButton hero status="danger" *ngIf="value == 'O'">{{ value }}</button>
+  `,
+  styles: [
+    'button { width: 100%; height: 100%; font-size: 5em !important; }'
+  ]
+})
+export class SquareComponent {
+
+  @Input() value: 'X' | 'O';
+
+}
